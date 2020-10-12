4a23b1f1d424d4f5d36cf39ef1c7d7465b05ec6f
dasha <dashka.didyk@gmail.com>
2020-10-12 21:22:01 +0300

    26

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-const { last } = require("lodash");
+import { last } from "lodash";
+import drawData  from "./utils";  //дефолтные значения импортируются без скобок 
 
 const apiUrl = 'http://localhost:3000';
 
 const addBtn = document.getElementById('add'),
     deleteBtn = document.getElementById('delete'),
     wrapper = document.getElementById('data');
-
-const createDataElement = (element) => `<div>
-    <span>ID: ${element.id}</span>
-    <span>Name: ${element.name}</span>
-    <span data-user-id="${element.id}" class="delete">X</span>
-</div>`;
-
-const createData = (data) =>
-    data.map((element) => createDataElement(element)).join('');
-
-const drawData = (data, wrapper = document.body) =>
-    (wrapper.innerHTML = createData(data));
 
 fetch(apiUrl)
     .then((response) => response.json())
